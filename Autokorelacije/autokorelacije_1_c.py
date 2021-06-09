39a6c51cd8d831f966a40246d7760733ac1c342f
madamovic <madamovickrivaja@gmail.com>
2021-06-09 15:58:41 +0200

    Autocorrelations codes, local and cluster updates

---
+++ Autokorelacije/autokorelacije_1_c.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import pyalps
+import matplotlib.pyplot as plt
+import pyalps.plot
+
+#prepare the input parameters
+parms = []
+for l in [2,4,8,16,32,48,64]:
+    parms.append(
+        { 
+          'LATTICE'        : "square lattice", 
+          'T'              : 2.26919,
+          'J'              : 1 ,
+          'THERMALIZATION' : 100000,
+          'SWEEPS'         : 1000000,
+          'UPDATE'         : "local",
+          'MODEL'          : "Ising",
+          'L'              : l
+        }
+    )
+
+#write the input file and run the simulation
+input_file = pyalps.writeInputFiles('parm1c',parms)
+pyalps.runApplication('spinmc',input_file,Tmin=5)
+
+#load the binning analysis for the absolute value of the magnetization
+binning = pyalps.loadBinningAnalysis(pyalps.getResultFiles(prefix='parm1c'),'|Magnetization|')
+binning = pyalps.flatten(binning)
+
+#make one plot with all data
+for dataset in binning:
+    dataset.props['label'] = 'L='+str(dataset.props['L'])
+
+plt.figure()
+plt.xlabel('binning nivo')
+plt.ylabel(u'Greška za magnetizaciju')
+plt.title(u'Bining analiza za 2D Izingov model na kvadratnoj rešetki, local updates')
+pyalps.plot.plot(binning)
+plt.legend(loc="best")
+plt.savefig("bining_local_3.eps",dpi=300)
+plt.show()
+
+
+
---
+++ Autokorelacije/autokorelacije_2_b.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import pyalps
+import matplotlib.pyplot as plt
+import pyalps.plot
+
+#prepare the input parameters
+parms = []
+for l in [2,4,8,16,32,48,64]:
+    parms.append(
+        { 
+          'LATTICE'        : "square lattice", 
+          'T'              : 2.26919,
+          'J'              : 1 ,
+          'THERMALIZATION' : 50000,
+          'SWEEPS'         : 100000,
+          'UPDATE'         : "cluster",
+          'MODEL'          : "Ising",
+          'L'              : l
+        }
+    )
+
+#write the input file and run the simulation
+input_file = pyalps.writeInputFiles('parm2b',parms)
+pyalps.runApplication('spinmc',input_file,Tmin=5)
+
+#load the binning analysis for the absolute value of the magnetization
+binning = pyalps.loadBinningAnalysis(pyalps.getResultFiles(prefix='parm2b'),'|Magnetization|')
+binning = pyalps.flatten(binning)
+
+#make one plot with all data
+for dataset in binning:
+    dataset.props['label'] = 'L='+str(dataset.props['L'])
+
+plt.figure()
+plt.xlabel('binning nivo')
+plt.ylabel(u'Greška za magnetizaciju')
+plt.title(u'Bining analiza za 2D Izingov model na kvadratnoj rešetki, cluster updates')
+pyalps.plot.plot(binning)
+plt.legend(loc="best")
+plt.savefig("bining_cluster_2.eps",dpi=300)
+plt.show()
+
